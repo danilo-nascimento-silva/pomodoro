e157f04629a60b7980850d7bc6b9b4d7bd4a00da
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-02-15 16:59:15 -0300

    Biblioteca nova
    Foi implementada uma nova biblioteca para interface

--- janela_teste.py
+++ janela_teste.py
@@ -1,22 +1,38 @@
-from tkinter import *
-import tkinter
-import time
+import PySimpleGUI as sg
 
-janela = Tk()
-janela.title('Pomodoro')
-janela.geometry('950x350+500+200')
-janela.resizable(False, False)
-janela.iconbitmap("imagens/pomodoro_icon.ico")
-janela['bg'] = '#97CDB5'
-photo = PhotoImage(file='imagens/tomate.png')
+def janela_inicial():
+    sg.theme('Reddit')   
+    layout = [  [sg.Text('Get Ready')],
+                [sg.Text('00:00')],
+                [sg.Button('Play')] ]
 
-label_relogio = Label(janela, text='00:00', font=('Times 150 bold'), fg='#DB524D', bg='#97CDB5')
-label_relogio.place(x=50,y=50)
+    return sg.Window('Inicial', layout=layout, finalize=True)
 
-botao_play = Button(janela, text='Play', width=10, height=2, bg='#FF6242', fg='white', font=('Ivy 12 bold'), relief='raised', overrelief='ridge')
-botao_play.place(x=650, y=270)
-botao_pause = Button(janela, text='Pause', width=10, height=2, bg='#FF6242', fg='white', font=('Ivy 12 bold'), relief='raised', overrelief='ridge')
-botao_pause.place(x=770, y=270)
+def janela_focus():
+    sg.theme('Reddit')   
+    layout = [  [sg.Text('Focus Time')],
+                [sg.Text('00:00')],
+                [sg.Button('Play'), sg.Button('Pause'), sg.Button('Restart')] ]
 
-janela.mainloop()
+    return sg.Window('Focus', layout=layout, finalize=True)
 
+def janela_break():
+    sg.theme('Reddit')   
+    layout = [  [sg.Text('Break Time')],
+                [sg.Text('00:00')],
+                [sg.Button('Play'), sg.Button('Pause'), sg.Button('Restart')] ]
+
+    return sg.Window('Break', layout=layout, finalize=True)
+
+janela1, janela2, janela3 = janela_inicial(), None, None
+
+while True:
+    window, event, values = sg.read_all_windows()
+
+    if window == janela1 and event == sg.WIN_CLOSED: 
+        break
+    if window == janela1 and event == 'Play':
+        janela2 = janela_focus()
+        janela1.hide()
+
+window.close()
--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from tkinter import *
+import PySimpleGUI as sg
 import time
-
-timer = float(input('Digite quantidade de tempo em minutos: '))
-timer = int(timer*60)
-p_curta = 5*60
-p_longa = 15*60
-contador = 0
-
-janela = Tk()
-janela.title('Pomodoro')
-janela.geometry('950x350+500+200')
-janela.resizable(False, False)
-janela.iconbitmap("imagens/pomodoro_icon.ico")
-janela['bg'] = '#97CDB5'
-photo = PhotoImage(file='imagens/tomate.png')
+init_temp = time.time()
 
 def pausa_c():
     init_temp = time.time()
@@ -41,13 +28,10 @@ def pausa_l():
             time.sleep(1)
     return
 
-def play():
-    init_temp = time.time()
-    pausas = 0
+def prin():
     while True:
-        global contador
         contador = int(time.time() - init_temp)
-        if  pausas == 2 and contador == timer:
+        if  pausas == 4 and contador == timer:
             pausa_l()
             pausas = 0
             init_temp = time.time()
@@ -58,13 +42,48 @@ def play():
         else:
             print(contador)
             time.sleep(1)
+    return
 
-      
-botao_play = Button(janela, text='Play', command=play,width=10, height=2, bg='#FF6242', fg='white', font=('Ivy 12 bold'), relief='raised', overrelief='ridge')
-botao_play.place(x=650, y=270)
-botao_pause = Button(janela, text='Pause', width=10, height=2, bg='#FF6242', fg='white', font=('Ivy 12 bold'), relief='raised', overrelief='ridge')
-botao_pause.place(x=770, y=270)
-label_relogio = Label(janela, text='00:00', font=('Times 150 bold'), fg='#DB524D', bg='#97CDB5')
-label_relogio.place(x=50,y=50)
 
-janela.mainloop()
+def janela_inicial():
+    sg.theme('Reddit')   
+    layout = [  [sg.Text('Get Ready')],
+                [sg.InputText(size=(10,1), key='timer')],
+                [sg.Button('Play')] ]
+
+    return sg.Window('Inicial', layout=layout, finalize=True)
+
+def janela_focus():
+    sg.theme('Reddit')   
+    layout = [  [sg.Text('Focus Time')],
+                [sg.Text(time.time())],
+                [sg.Button('Play'), sg.Button('Pause'), sg.Button('Restart')] ]
+
+    return sg.Window('Focus', layout=layout, finalize=True)
+
+def janela_break():
+    sg.theme('Reddit')   
+    layout = [  [sg.Text('Break Time')],
+                [sg.Text('00:00')],
+                [sg.Button('Play'), sg.Button('Pause'), sg.Button('Restart')] ]
+
+    return sg.Window('Break', layout=layout, finalize=True)
+
+p_curta = 5*60
+p_longa = 15*60
+pausas = 0
+
+janela1, janela2, janela3 = janela_inicial(), None, None
+timer = None
+
+while True:
+    window, event, values = sg.read_all_windows()
+    if window == janela1 and event == sg.WIN_CLOSED: 
+        break
+    if window == janela2 and event == sg.WIN_CLOSED: 
+        break
+    if window == janela1 and event == 'Play':
+        janela1.hide()
+        janela2 = janela_focus()
+        
+window.close()
