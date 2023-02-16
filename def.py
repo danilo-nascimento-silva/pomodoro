b4fd7885edd30bddfd41514a00ad74a68f45e190
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-02-16 16:48:40 -0300

    Novo método
    Novo método para temporizador, alterações feito no arquivo def.py, após test vou passar para o principal e seguir com o desenvolvimento com essa nova métodologia

--- def.py
+++ def.py
@@ -1,22 +1,47 @@
-import PySimpleGUI as sg
+import time
+
+t = input("Digite o tempo (em segundos): ")
+pomodoro = t
+pausa_c = 5
+pausa_l = 10
+pausas = 0
+
+if t.isdigit():
+    t = int(t)
+    pomodoro = int(t)
+else:
+    print("Entrada inválida!")
+    quit()
+
+while True: 
+    minutes, seconds = divmod(t, 60)    
+    timer = "{:02d}:{:02d}".format(minutes, seconds)
+    print(timer, end="\r")
+    time.sleep(1)
+    t = t - 1
+
+    if t == 0 and pausas == 4:
+        print('Pausa Longa')
+        pausas = 0  
+        while pausa_l != 0:
+            minutes, seconds = divmod(pausa_l, 60)    
+            timer = "{:02d}:{:02d}".format(minutes, seconds)
+            print(timer, end="\r")
+            time.sleep(1)
+            pausa_l -= 1
+        print('Acabou a pausa curta')
+        t = pomodoro
+
+    if t == 0:
+        print('Pausa Curta')
+        pausas += 1 
+        while pausa_c != 0:
+            minutes, seconds = divmod(pausa_c, 60)    
+            timer = "{:02d}:{:02d}".format(minutes, seconds)
+            print(timer, end="\r")
+            time.sleep(1)
+            pausa_c -= 1
+        print('Acabou a pausa curta')
+        t = pomodoro
 
 
-layout = [
-    [sg.Text("Pegar Cotação da Moeda")],
-    [sg.InputText(key="nome_cotacao")],
-    [sg.Button("Pegar Cotação"), sg.Button("Cancelar")],
-    [sg.Text("", key="texto_cotacao")],
-]
-
-janela = sg.Window("Sistema de Cotações", layout)
-
-while True:
-    evento, valores = janela.read()
-    if evento == sg.WIN_CLOSED or evento == "Cancelar":
-        break
-    if evento == "Pegar Cotação":
-        codigo_cotacao = valores["nome_cotacao"]
-
-        janela["texto_cotacao"].update(f"A cotação do {codigo_cotacao} é de R${''}")
-
-janela.close()
