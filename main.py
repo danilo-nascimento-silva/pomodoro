a44f3c8376826fc3c227b4d49c173ae11c28150e
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-02-19 20:12:01 -0300

    Interface
    Aplicação ja se comunica com sua interface de maneira satisfatória.

--- main.py
+++ main.py
@@ -1,100 +1,136 @@
 import PySimpleGUI as sg #Biblioteca para interface 
 import time #Biblioteca para contar tempo
+from playsound import playsound
+
+# Verifica se a string representa um inteiro
+
+def isInt(value):
+    value_int = None
+    try:
+        value_int = int(value)
+        if value_int is not None:
+            return True
+    except:
+        return False
+
 # ------------------------------------ Função que mantém a pagina inicial ------------------------------------
 def janela_inicial():
     sg.theme('Reddit')   # Tema da pagia
     layout = [  [sg.Text('Get Ready')], # Nome topo da pagina
-                [sg.InputText(size=(10,1), key='timer_')], # Caixa de texto que recebe a quantidade de tempo
-                [sg.Button('Play')] ] # Botão para o iniciar 
+                [sg.Text('Tempo de Foco:  '), sg.InputText(size=(9,1), key='timer_')], # Caixa de texto que recebe a quantidade de tempo
+                #[sg.Text('Pausa Curta:      '), sg.InputText(size=(9,1), key='focus_c')],
+                #[sg.Text('Pausa Longa:     '), sg.InputText(size=(9,1), key='focus_l')], # Caixa de texto que recebe a quantidade de tempo
+                [sg.Button('Play', size=(22, 1))] ] # Botão para o iniciar 
 
     return sg.Window('Inicial', layout=layout, finalize=True)
 # ------------------------------------ Função que mantém a pagina de foco ------------------------------------
-def janela_focus():
+def janela_focus(t_, p_):
     sg.theme('DarkRed2')   # Tema da pagia
-    layout = [  [sg.Text('Focus Time')], # Nome topo da pagina
-                [sg.Text(t)], # Texto que recebe a variável de tempo 
+    layout = [  [sg.Text('Focus Time               '), (sg.Text(p_, key='qtd_focus'))], # Nome topo da pagina
+                [sg.Text(t_, key='time_focus_')], # Texto que recebe a variável de tempo 
                 [sg.Button('Play'), sg.Button('Pause'), sg.Button('Restart')] ] # Botões de Play, pause e Restart 
 
     return sg.Window('Focus', layout=layout, finalize=True)
 # ------------------------------------ Função que mantém a pagina de descanso ------------------------------------
-def janela_break():
+def janela_break(p_, q):
     sg.theme('DarkGreen')   # Tema da pagia
-    layout = [  [sg.Text('Break Time')], # Nome topo da pagina
-                [sg.Text(t)], # Texto que recebe a variável de tempo 
-                [sg.Button('Play'), sg.Button('Pause'), sg.Button('Restart')] ] # Botões de Play, pause e Restart 
+    layout = [  [sg.Text('Break Time     '), (sg.Text(q, key='qtd_focus'))], # Nome topo da pagina
+                [sg.Text(p_, key='time_pause_')], # Texto que recebe a variável de tempo 
+                [sg.Button('Play', size=(15, 1))]] # Botão de Play
 
     return sg.Window('Break', layout=layout, finalize=True)
 
+def convert_temp(i):
+    minutos, segundos = divmod(i, 60)
+    return"{:02d}:{:02d}".format(minutos, segundos)
 janela1, janela2, janela3 = janela_inicial(), None, None # Variáveis que recebe as paginas 
 
-t = 0 # Declarar a variável de tempo que é atualizada no loop 
-pomodoro = t # Guarda o tempo desejado 
-pausa_c = 5 #300 5 minutos para a pausa curta
-pausa_l = 10 #1200 15 minutos para a pausa longa
-pausas = 0  # Variável que armazena a quantidade de pausas curta para chamar a pausa longa
-mudar_tela = False # Variével que muda a tela nas pausas
+t = 0 # guarda o tempo de foco no decorrer do código
+p = 0 # guarda o tempo de pausa no decorrer do código
+quant_p = 0
+pomodoro = 0 # Guarda o tempo inicial configurado para foco 
+
+flag_decrement_time_focus = False
+time_init_focus = None
+
+flag_decrement_time_pause = False
+time_init_pause = None
+
+last_window = None
+
 
 while True:
+    window, event, values = sg.read_all_windows(timeout=100)
+    if window is not None:
+        last_window = window
+    else: 
+        window = last_window
 
-    window, event, values = sg.read_all_windows() 
 # ------------------------------------ Condições que verifica se a aplicação foi fechada ------------------------------------
 
     if window == janela1 and event == sg.WIN_CLOSED: 
         break
     if window == janela2 and event == sg.WIN_CLOSED:
-        break 
-    if window == janela3 and event == sg.WIN_CLOSED:
-        break 
+        janela1.un_hide()
+        janela2.close() 
+        flag_decrement_time_focus = False
 
+    if window == janela3 and event == sg.WIN_CLOSED:
+        janela1.un_hide()
+        janela3.close() 
+        flag_decrement_time_focus = False
+    
 # ---------------------------------------------------------------------------------------------------------------------------
 
+    if window == janela3 and event == 'Play':
+        time_init_pause = time.time() # Salva o start
+        flag_decrement_time_pause = True # Flag que diz que a contagem está habilitada
+        p = int(window['time_pause_'].get())
+    
     if window == janela1 and event == 'Play': # Condição que verifica se a janela de apresentação esta aberta e se o botão play foi pressionado
-        janela1.hide() # Fecha a janela de apresentação
-        janela2 = janela_focus() # Abre a janela de Foco 
-        t = int(values['timer_'])*60 # Atribui a variavel "t" o valor digitado na tela de apresentação 
-        pomodoro = int(t) # Guarda o valor atribuído em outra variável para salvar a informação e usar no término do loop de pausa
+        if isInt(values['timer_']):
+            if int(values['timer_']) <= 60:
+                janela1.hide() # Oculta a janela de apresentação
+                t = int(values['timer_'])*60 # Atribui a variavel "t" o valor digitado na tela de apresentação 
+                janela2 = janela_focus(t, quant_p) # Abre a janela de Foco e passa o tempo como parâmetro
+                pomodoro = t # Guarda o valor atribuído em outra variável para salvar a informação e usar no término do loop de pausa
 
     if window == janela2 and event == 'Restart': # Verifica se o botão de restart foi pressionado na tela de foco
-        print('Reset')
-        t = pomodoro # Reseta o tempo, atribuído o valor que foi salvo na variavel Pomodoro 
+        window['time_focus_'].update(pomodoro) # Reseta o tempo, atribuído o valor que foi salvo na variavel Pomodoro 
+        flag_decrement_time_focus = False
 
-    if window == janela2 and mudar_tela: # Verifica se a janela de foco esta aberta e se a variável for True
-        janela2.hide() # Fecha a janela de foco
-        janela3 = janela_break() # Abre a janela de pausa
+    if window == janela2 and event == 'Play': # Se play, habilita decrementar tempo de foco
+        time_init_focus = time.time() # Salva o start
+        flag_decrement_time_focus = True # Flag que diz que a contagem está habilitada
+        t = int(window['time_focus_'].get())
+    
+    if window == janela2 and event == 'Pause': 
+        flag_decrement_time_focus = False # Flag que diz que a contagem está desabilitada
+    
+    if flag_decrement_time_focus:
+        running_time = int(time.time() -time_init_focus)
+        window['time_focus_'].update(convert_temp(t - running_time))
+            
+        if t -running_time < 0:
+            janela2.close()
+            if quant_p == 3:
+                janela3 = janela_break(900, quant_p)
+                quant_p = 0
+            else:
+                janela3 = janela_break(300, quant_p)
+            quant_p += 1
+            flag_decrement_time_focus = False
+    
+    if flag_decrement_time_pause:
+        running_time = int(time.time() -time_init_pause)
+        window['time_pause_'].update(convert_temp(p -running_time))
+            
+        if p -running_time < 0 :
+            janela3.close()
+            janela2 = janela_focus(pomodoro, quant_p)
+            flag_decrement_time_pause = False
 
-    minutes, seconds = divmod(t, 60) # Recebe a informação de tempo em segundos, divide para transformar de segundos para minutos e atribui o- 
-    # valor da divisão inteira para a variável "Minutos" e o resto da divisão para a variével "Segundos"
-    timer = "{:02d}:{:02d}".format(minutes, seconds) # Monta o formato de print para ser impresso
-    print(timer, end="\r") # Printa a variável s
-    t -= 1 # A cada segundo decrementa 1 segundo da variável "t"
-    time.sleep(1) # Segura o codigo por 1 segundo 
 
-    if t == 0 and pausas == 4: # Condição que verifica se a variável "t" chegou a zero e se a variável pausas chegou a 4
-        # isso significa que ja tiveram 4 pausas curtas e ta na hora que chamar uma pausa longa
-        print('Pausa Longa')
-        pausas = 0 # Zera a quantidade de pausas 
-        mudar_tela = True  
-        while pausa_l != 0:
-            minutes, seconds = divmod(pausa_l, 60)    
-            timer = "{:02d}:{:02d}".format(minutes, seconds)
-            print(timer, end="\r")
-            time.sleep(1)
-            pausa_l -= 1
-        print('Acabou a pausa curta')
-        t = pomodoro
-        mudar_tela = False 
 
-    if t == 0: # Condição que verifica se a variável "t" chegou a zero para chamar uma pausa curta
-        print('Pausa Curta')
-        pausas += 1 # Atribui +1 na variavel para verificar se esta na hora de uma pausa longa
-        mudar_tela = True   
-        while pausa_c != 0:
-            minutes, seconds = divmod(pausa_c, 60)    
-            timer = "{:02d}:{:02d}".format(minutes, seconds)
-            print(timer, end="\r")
-            time.sleep(1)
-            pausa_c -= 1
-        print('Acabou a pausa curta')
-        t = pomodoro
-        mudar_tela = False
+
 
